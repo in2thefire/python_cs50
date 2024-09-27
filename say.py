04f7342f06781f035c6f097dda7ca72be93d56f5
Bohdan Kyrylko <in2thefire.code@gmail.com>
2024-09-27 18:05:34 +0300

    creating my own module for python

--- say.py
+++ say.py
@@ -2,4 +2,4 @@ import cowsay
 import sys
 
 if len(sys.argv) == 2:
-    cowsay.cow("Hello, " + sys.argv[1])
+    cowsay.trex("Hello, " + sys.argv[1])
---
+++ saying.py
@@ -0,0 +1,11 @@
+def main():
+    hello("Bohdan")
+    goodbye("Bohdan")
+
+def hello(name):
+    print(f"Hello, {name}")
+
+def goodbye(name):
+    print(f"Goodbye, {name}")
+if __name__ == "__main__":
+    main()
