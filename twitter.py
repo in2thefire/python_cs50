5e222ee1644d15ca1fbde85b546a7753ab34acb2
Bohdan Kyrylko <in2thefire.code@gmail.com>
2024-10-06 22:07:35 +0200

    twitter url throw re.search

--- twitter.py
+++ twitter.py
@@ -2,5 +2,10 @@ import re
 url = input("URL: ").strip()    
 #username = url.removeprefix("https://twitter.com/")
 
-username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
-print(f"Username: {username}")
+#username = re.sub(r"^(https?://)?(www\.)?twitter\.com/", "", url)
+#print(f"Username: {username}")
+
+if matches := re.search(r"^(?:https?://)?(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+    print(f"Username: {matches.group(1)}")
+else:
+    print("URL is incorect")
