361b51bf8633aac8a9220cebba0377b014034904
Bohdan Kyrylko <in2thefire.code@gmail.com>
2024-10-09 19:33:24 +0200

    CLASSES!!!

--- student_oop.py
+++ student_oop.py
@@ -1,16 +1,26 @@
+class Student:
+    ...
+
+
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Rawenclaw"
+    if student.name == "Padma":
+        student.house = "Rawenclaw"
     '''if student[0] == "Padma":
         student[1] = "Rawenclaw"'''
     
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
+
+'''def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return {"name": name, "house": house}
+    return {"name": name, "house": house}'''
 
 '''def get_student():
     student = {}
