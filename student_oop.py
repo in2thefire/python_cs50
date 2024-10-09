cf3f5d3f345d053363f9e96433fe05177143c839
Bohdan Kyrylko <in2thefire.code@gmail.com>
2024-10-09 19:20:49 +0200

    final version with dicts

--- student_oop.py
+++ student_oop.py
@@ -1,14 +1,23 @@
 def main():
     student = get_student()
+    if student["name"] == "Padma":
+        student["house"] = "Rawenclaw"
     '''if student[0] == "Padma":
         student[1] = "Rawenclaw"'''
+    
     print(f"{student['name']} from {student['house']}")
 
 def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
+
+'''def get_student():
     student = {}
     student["name"] = input("Name: ")
     student["house"] = input("House: ")
-    return student
+    return student'''
+    
 '''def get_student():
     name = input("Name: ")
     house = input("House: ")
